35a47f2e7104a72e92a935b216128b5971a40c86
jaska16491 <s16491@pjwstk.edu.pl>
2020-01-22 14:52:24 +0100

    Dodaj Get

---
+++ PizzaApp/Controllers/SkladnikisController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PizzaApp.Models;
+
+namespace PizzaApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SkladnikisController : ControllerBase
+    {
+        private s16491Context _context;
+        public SkladnikisController(s16491Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Getskladnikis()
+        {
+            return Ok(_context.Skladnik.ToList());
+        }
+
+        [HttpGet("{idSkladnik:int}")]
+        public IActionResult GetSkladnik(int idSkladnik)
+        {
+            var skladnik = _context.Skladnik.FirstOrDefault(p => p.IdSkladnik == idSkladnik);
+
+            if (skladnik == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(skladnik);
+        }
+    }
+}
---
+++ PizzaApp/Controllers/ZamowieniesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PizzaApp.Models;
+
+namespace PizzaApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ZamowieniasController : ControllerBase
+    {
+        private s16491Context _context;
+        public ZamowieniasController(s16491Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetZamowienias()
+        {
+            return Ok(_context.Zamowienie.ToList());
+        }
+
+        [HttpGet("{idZamowienie:int}")]
+        public IActionResult GetZamowienie(int idZamowienie)
+        {
+            var zamowienie = _context.Zamowienie.FirstOrDefault(p => p.IdZamowienie == idZamowienie);
+
+            if (zamowienie == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(zamowienie);
+        }
+    }
+}
